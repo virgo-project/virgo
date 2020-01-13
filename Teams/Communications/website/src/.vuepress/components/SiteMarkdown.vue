ad22978eeb336f67f6c5374d8d832aec7c7366da
Peng Zhong <172531+nylira@users.noreply.github.com>
2020-01-13 17:49:16 -0500

    remove extra styles

--- Teams/Communications/website/src/.vuepress/components/SiteMarkdown.vue
+++ Teams/Communications/website/src/.vuepress/components/SiteMarkdown.vue
@@ -4,38 +4,12 @@
   </div>
 </template>
 
-<style lang="stylus">
+<style scoped lang="stylus">
 #site-markdown
   flex 1
   padding 3rem 1rem
   margin-top 3rem
 
-h2,
-h3,
-h4,
-h5,
-h6
-  margin-top 3rem
-
-h2
-  border-top 1px solid var(--bc)
-  padding-top 3rem
-
-h3
-  font-weight 400
-
-h4
-  font-family var(--serif)
-
-p
-  margin 1.5em 0
-  &:last-child
-    margin-bottom 0
-
-li
-  margin-bottom 0.5rem
-  p
-    margin 0
 @media screen and (min-width: 768px)
   #site-markdown
     margin-left 12.5vw
